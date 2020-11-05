9f14c40cd89aed1c6544e16fd67f7dbd182bbdfc
James Smith <james.smith9113@gmail.com>
2020-11-05 12:25:08 -0700

    Changing pip install

--- osx/install_llvm.sh
+++ osx/install_llvm.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Get prereqs
-python3 -m pip install --user cmake ninja
+pip install cmake ninja
+
+# Test that pip install modules are in $PATH
+cmake --version
+ninja --version
 
 # Get sources
 git clone https://github.com/llvm/llvm-project.git
@@ -10,4 +14,5 @@ git clone https://github.com/llvm/llvm-project.git
 cd llvm-project
 mkdir build && cd build
 cmake -G Ninja ../llvm -DCMAKE_INSTALL_PREFIX=$(pwd)/darwin_build -DCMAKE_BUILD_TYPE=Release
+ninja
 ninja install
