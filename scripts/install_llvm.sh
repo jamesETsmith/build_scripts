2697acf4dfb89cacfdb3d1d357599f493e35611a
James Smith <james.smith9113@gmail.com>
2020-11-05 15:44:42 -0700

    Updating apt-get before install of prereqs

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -7,6 +7,7 @@
 FILE=/etc/os-release
 if test -f "$FILE"; then
     echo "$FILE exists. We're probably on a Linux system."
+    sudo apt-get update -y
     sudo apt-get install -y cmake ninja
 else 
     echo "$FILE does not exist. We're probably not on Linux."
